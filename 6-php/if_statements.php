f6c34877230b7607ccf49177c331ca26132e776e
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-04-11 23:45:11 +0900

    PHP if statements

---
+++ 6-php/if_statements.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = "bob";
+if ($user == "bob"){
+    echo "Hello Bob";
+} else {
+    echo "I don't know you";
+}
+echo "<br>";
+$age = 25;
+if ($age >= 18){
+    echo "Welcome to the club";
+} else {
+    echo "You're too young to join";
+}
+?>
