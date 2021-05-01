350f69cb2eee082bc8b0dc3a1abae162bba1a899
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-05-01 15:06:07 +0900

    PHP database get and update

---
+++ 7-mysql/index.php
@@ -0,0 +1,30 @@
+<?php
+
+   
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    // Inserting data
+    //$query = "INSERT INTO `users` (`email`, `password`) VALUES('bob@webdev.com','yayitsworking')";
+
+    // Updating data
+    //$query = "UPDATE `users` SET email = 'frank@webdev.com' WHERE id = 1 LIMIT 1"; // By ID
+    $query = "UPDATE `users` SET password = '18shc73h5' WHERE email = 'frank@webdev.com' LIMIT 1";
+    mysqli_query($conn, $query);
+
+    // Get data
+    $query = "SELECT * FROM users";
+
+    if ($result = mysqli_query($conn, $query)){
+        $row = mysqli_fetch_array($result);
+        echo "Your email is ".$row[1]." and your password is ".$row[2];
+    }
+
+    mysqli_close($conn);
+?>
