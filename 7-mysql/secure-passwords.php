e8a25ee5ad5ac7cdf157d3e814a739da98b3e155
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2021-05-03 19:59:30 +0900

    PHP hash()

--- 7-mysql/secure-passwords.php
+++ 7-mysql/secure-passwords.php
@@ -15,4 +15,21 @@
 
     // Note there are still higher forms of security available.
 
+    /* The PHP5.5 hash method below */
+    // Generate a hash of the password "mypassword"
+    $hash = password_hash("mypassword", PASSWORD_DEFAULT);
+ 
+    // Echoing it out, so we can see it:
+    echo $hash;
+ 
+    // Some line breaks for a cleaner output:
+    echo "<br><br>";
+ 
+    // Using password_verify() to check if "mypassword" matches the hash.
+    // Try changing "mypassword" below to something else and then refresh the page.
+    if (password_verify('mypassword', $hash)) {
+        echo 'Password is valid!';
+    } else {
+        echo 'Invalid password.';
+    }
 ?>
